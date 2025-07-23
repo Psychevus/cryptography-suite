77087bc56eda1048ac260a2f1a9a51a58fb629e7
Psychevus <Psychevus@gmail.com>
2025-07-23 08:07:11 +0330

    Add SPHINCS+ signature support

--- tests/test_pqc.py
+++ tests/test_pqc.py
@@ -2,12 +2,16 @@ import unittest
 
 from cryptography_suite.pqc import (
     PQCRYPTO_AVAILABLE,
+    SPHINCS_AVAILABLE,
     generate_kyber_keypair,
     kyber_encrypt,
     kyber_decrypt,
     generate_dilithium_keypair,
     dilithium_sign,
     dilithium_verify,
+    generate_sphincs_keypair,
+    sphincs_sign,
+    sphincs_verify,
 )
 
 
@@ -30,6 +34,23 @@ class TestPQC(unittest.TestCase):
         sig = dilithium_sign(sk, msg)
         self.assertIsInstance(sig, str)
         self.assertTrue(dilithium_verify(pk, msg, sig))
+
+    @unittest.skipUnless(SPHINCS_AVAILABLE, "SPHINCS+ not available")
+    def test_sphincs_signature(self):
+        pk, sk = generate_sphincs_keypair()
+        msg = b"sphincs test"
+        sig = sphincs_sign(sk, msg)
+        self.assertIsInstance(sig, bytes)
+        self.assertTrue(sphincs_verify(pk, msg, sig))
+
+    @unittest.skipUnless(SPHINCS_AVAILABLE, "SPHINCS+ not available")
+    def test_sphincs_negative(self):
+        pk, sk = generate_sphincs_keypair()
+        msg = b"hello"
+        sig = sphincs_sign(sk, msg)
+        self.assertFalse(sphincs_verify(pk, b"bye", sig))
+        pk2, _ = generate_sphincs_keypair()
+        self.assertFalse(sphincs_verify(pk2, msg, sig))
 
 
 if __name__ == "__main__":
